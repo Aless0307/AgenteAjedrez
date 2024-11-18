b6f3d1fa84fe826318d42d3bba496e561919a030
dfdez2003 <df4720350@gmail.com>
2024-11-18 12:14:54 -0600

    Update ajedrez.py
    qpd cambio 1

--- ajedrez.py
+++ ajedrez.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Set, Tuple, Optional
 import numpy as np
 from piezas import GestorPiezas, MovimientosTablero, MovimientoAjedrez, TipoMovimiento
 
-# hoal tilin
+# hola tilinnnnnnnn
 class AgenteAjedrez:
     # Constantes para piezas
     VACIO = '...'
